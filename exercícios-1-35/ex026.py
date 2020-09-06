2989734bf0a5e5cb84e54b8837439b0dc3da954c
CorSar <1cesar.ccorreia@gmail.com>
2020-09-07 00:11:15 +0100

    Update ex026.py

--- exercícios-1-35/ex026.py
+++ exercícios-1-35/ex026.py
@@ -3,7 +3,6 @@ print('A letra "A" no seu texto aparece {} vezes'.format(frase.count('A')))
 print('Aparece pela 1ª vez na posição {}'.format(frase.find('A')+1))
 print('E aparece pela última na {} posição'.format(frase.rfind('A')+1))
 
-
 frase = str(input('Digite uma frase:')).strip().lower()
 print('Existem {} "a" na sua frase'.format(frase.count('a')) )
 print('O primeiro "a" aparece nas posição {}'.format(frase.find('a')+1))
